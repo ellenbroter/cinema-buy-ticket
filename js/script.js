da7533b1109e55993eee73687caa94174f41a56c
Ellen Brøter <143997330+ellenbroter@users.noreply.github.com>
2023-11-14 09:26:20 +0100

    Log value from inputs

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
-const logFormInput = () => {
-  // Get form values
-  const firstName = document.querySelector('.first-name').value;
-  const lastName = document.querySelector('.last-name').value;
-  const email = document.querySelector('.email').value;
-  const confirmEmail = document.querySelector('.confirm-email').value;
-  const mobilenumber = document.querySelector('.mobilenumber').value;
+const form = document.querySelector('.customer-details')
+const firstName = document.querySelector('.first-name')
+const lastName = document.querySelector('.last-name')
+const email = document.querySelector('.email')
+const confirmEmail = document.querySelector('.confirm-email')
+const mobilenumber = document.querySelector('.mobilenumber')
 
-  // Log the form input to the console
-  console.log(firstName, lastName, email, confirmEmail, mobilenumber);
-};
-
-// Prevent default submit button behavior
-const submitHandler = (event) => {
-  event.preventDefault();
-
-// Add an event listener to the button
-document.querySelector('.submit-button').addEventListener('click', logFormInput);
-};
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    console.log('Firstname: ', firstName.value)
+    console.log('Lastname: ', lastName.value)
+    console.log('E-mail: ', email.value)
+    console.log('Confirm e-mail: ', confirmEmail.value)
+    console.log('Mobile number: ', mobilenumber.value)
+});
